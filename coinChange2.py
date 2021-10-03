0decc500039bef93da60273e9cc0041b025417a7
ThanosKou <koutsaftis.t@gmail.com>
2021-10-03 02:11:59 -0400

    Update coinChange2.py

--- coinChange2.py
+++ coinChange2.py
@@ -8,3 +8,7 @@ class Solution:
                 if i>= coin:
                     dp[i] += dp[i-coin]
         return(dp[amount])
+
+    # Difference betweem coinChange2 (no duplicates) and combination sum (duplicates matter:order matters): In the comb sum for each position you try each coin. 
+    # In that way, you are distinguishing two different orderings. 
+    # If you instead loop through every position for each coin, then you are imposing a specific ordering
