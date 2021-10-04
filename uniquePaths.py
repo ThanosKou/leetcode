cb1a98fd06a3f91f75bacba31033c89e7f66ef9d
ThanosKou <koutsaftis.t@gmail.com>
2021-10-03 22:57:08 -0400

    Update uniquePaths.py

--- uniquePaths.py
+++ uniquePaths.py
@@ -2,9 +2,11 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
         
-        dp = [[1 for i in range(m)] for j in range(n) ]
-
+        dp = [1 for i in range(m)]
+        l = 1
         for i in range(1,n):
             for j in range(1,m):
-                dp[i][j] = dp[i][j-1] + dp[i-1][j]
-        return(dp[n-1][m-1])
+                # dp[i][j] = dp[i][j-1] + dp[i-1][j] =>
+                # dp[j] = dp[j-1] + dp[j] =>
+                dp[j] += dp[j-1]
+        return(dp[-1])
