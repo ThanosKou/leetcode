223ac9590d722d030d1b9bc8133a5dc5163147fb
ThanosKou <koutsaftis.t@gmail.com>
2021-10-11 02:20:15 -0400

    Update minWindSubstr.py

--- minWindSubstr.py
+++ minWindSubstr.py
@@ -1,21 +1,21 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
-        need = collections.Counter(t)            #hash table to store char frequency
-        missing = len(t)                         #total number of chars we care
-        start, end = 0, 0
-        i = 0
-        for j, char in enumerate(s, 1):          #index j from 1
-            if need[char] > 0:
-                missing -= 1
-            need[char] -= 1
-            if missing == 0:                     #match all chars
-                while i < j and need[s[i]] < 0:  #remove chars to find the real start
-                    need[s[i]] += 1
-                    i += 1
-                need[s[i]] += 1                  #make sure the first appearing char satisfies need[char]>0
-                missing += 1                     #we missed this first char, so add missing by 1
-                if end == 0 or j-i < end-start:  #update window
-                    start, end = i, j
-                i += 1                           #update i to start+1 for next window
-        return s[start:end]
+       
+      from collections import Counter
+  
+      need, missing = Counter(t), len(t) # need shows how many occur of each character we need, missing indicates when valid substring
+      I, J = 0, 0
+      i = 0
+
+      for j, ch in enumerate(s,1):
+        if need[ch] > 0:
+          missing -= 1
+        need[ch] -= 1 # we don't care for negative appearances, it shows we dont need that character
+        if not missing:
+          while i < j and need[s[i]] < 0:
+            need[s[i]] += 1
+            i += 1
+          if j - i <= J - I or not J:
+              J, I = j, i
+
+      return(s[I:J])
