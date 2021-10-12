47b34c1cb30c23e9ad7436c24eb9ac8feb54282d
ThanosKou <koutsaftis.t@gmail.com>
2021-10-12 19:51:58 -0400

    Update middle-of-the-linked-list.py

--- middle-of-the-linked-list.py
+++ middle-of-the-linked-list.py
@@ -12,3 +12,22 @@ class Solution:
         for i in range(lenList//2):
             head = head.next
         return(head)
+    
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        
+        fast = slow = head
+        
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next 
+        
+        if fast.next:
+            return(slow.next)
+        return(slow)
+    
+    # https://leetcode.com/problems/reorder-list/
