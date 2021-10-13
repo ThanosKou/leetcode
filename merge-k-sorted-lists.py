067ec415c73b5a10658b7d3e93e7598f4ff49a87
ThanosKou <koutsaftis.t@gmail.com>
2021-10-12 21:45:11 -0400

    Update merge-k-sorted-lists.py

--- merge-k-sorted-lists.py
+++ merge-k-sorted-lists.py
@@ -1,84 +1,25 @@
-class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        
-        ans = ListNode(0)
-        point = ans
-        arr = []
-        
-        for llist in lists:
-            while llist:
-                arr.append(llist.val)
-                llist = llist.next
-        
-        for i in sorted(arr):
-            point.next = ListNode(i)
-            point = point.next
-        
-        return(ans.next)
-		
-	from queue import PriorityQueue
+# With min heaps!
 
-### Using prioritty queue 
+class Solution(object):
+    def mergeKLists(self, lists):
 
-# class Solution:
-    # def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        dummy = ListNode(0)
+        curr = dummy
         
+        to_be_sorted = [(head.val, i, head) for i, head in enumerate(lists) if head]
+        heapify(to_be_sorted)
 
-        # ans = ListNode(0)
-        # point = ans
-        # arr = []
-        # q = PriorityQueue()
-        
-        # for llist in lists:
-            # while llist:
-                # q.put(llist.val)
-                # llist = llist.next
-        
-        # while not q.empty():
-            # q_val = q.get()
-            # point.next = ListNode(q_val)
-            # point = point.next
-            
-        # return(ans.next)
-		
-### Kinda like merge sort, merge lists by pairs  using merge2Lists 		
-		
-# class Solution(object):
-    # def mergeKLists(self, lists):
-        # """
-        # :type lists: List[ListNode]
-        # :rtype: ListNode
-        # """
-        # amount = len(lists)
-        # interval = 1
-        # while interval < amount:
-            # for i in range(0, amount - interval, interval * 2):
-                # lists[i] = self.merge2Lists(lists[i], lists[i + interval])
-            # interval *= 2
-        # return lists[0] if amount > 0 else None  
-        
-    # def merge2Lists(self, list1, list2):
-        # """
-        # :type list1: List[ListNode]
-        # :type list2: List[ListNode]
-        # :rtype: ListNode
-        # """      
-        # ans = ListNode(0)
-        # point = ans
-        
-        # while list1 and list2:
-            # if list1.val < list2.val:
-                # point.next = list1
-                # list1 = list1.next
-            # else:
-                # point.next = list2
-                # list2 = list2.next
-            # point = point.next
-                
-        # if not list1:
-            # point.next = list2
-        # if not list2:
-            # point.next = list1
-                
-        # return(ans.next)
+
+        while to_be_sorted != []:
+            val, i, node = to_be_sorted[0]
+            if not node.next:
+                heappop(to_be_sorted)
+            else:
+                heapreplace(to_be_sorted, (node.next.val, i, node.next))
+            curr.next = node
+            curr = curr.next
+        return(dummy.next)
+
+# https://leetcode.com/problems/merge-k-sorted-lists/
+
     
