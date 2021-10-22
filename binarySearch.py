b8ac781af2990d667d0a3edfe0f6ee68051722ce
ThanosKou <koutsaftis.t@gmail.com>
2021-10-22 17:38:31 -0400

    Create binarySearch.py

---
+++ binarySearch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            
+            pivot = left + (right-left)//2
+            if nums[pivot] == target:
+                return(pivot)
+            if nums[pivot] <= target:
+                left = pivot + 1
+            else:
+                right = pivot - 1
+        return(-1)
+
+# https://leetcode.com/problems/binary-search/
