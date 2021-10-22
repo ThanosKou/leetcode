0ffd5e1d7bb9972a2aabb27a3cf505a00b56c7ac
ThanosKou <koutsaftis.t@gmail.com>
2021-10-21 22:16:28 -0400

    Update subsets2.py

--- fb_jul20_jul21/subsets2.py
+++ fb_jul20_jul21/subsets2.py
@@ -11,8 +11,23 @@ class Solution:
                 res.append(sorted(cur_res))
         
         return(res)
-
-# https://leetcode.com/problems/subsets-ii/
-
-
+    
 ## Backtracking
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
+        nums.sort()
+        self.dfs(nums, res, [])
+        return(res)
+    
+    
+    def dfs(self, nums, res, path):
+        res.append(path)        
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums[i+1:], res, path + [nums[i]])
+            
+            
+       # https://leetcode.com/problems/subsets-ii/ 
