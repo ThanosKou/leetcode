e7abade27b588a49662636a5a1d1d9ceb5cfa936
ThanosKou <koutsaftis.t@gmail.com>
2021-10-23 18:10:45 -0400

    Create populateNextRightPointers.py

---
+++ fb_jul20_jul21/populateNextRightPointers.py
@@ -0,0 +1,43 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return(root)
+        queue = collections.deque([root])
+        level = collections.deque([root])
+        res = []
+        
+        while queue:
+            cur_level = []
+            queue = collections.deque([])
+            while level:
+                node = level.popleft()
+                cur_level.append(node)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level = queue
+            res.append(cur_level)
+        
+        # res contains all tree nodes grouped by level
+        
+        for level in res:
+            for i in range(len(level)-1):
+                level[i].next = level[i+1]
+            #level[-1].next = None
+        
+        return(root)
+
+
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
